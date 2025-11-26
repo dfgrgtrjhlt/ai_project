78a731203f0f7ecf309597eef8157cdd98d2a39f
dfgrgtrjhlt <wr0228781216@gmail.com>
2025-11-26 10:08:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st 
 st.title('나의 첫 웹 서비스 만들기!')
 a=st.text_input('이름을 입력하셈') 
-st.selectbox('니가 좋아하는 걸 말해보시긬ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ',['떡볶이'])
+st.selectbox('니가 좋아하는 걸 말해보세여',['떡볶이'])
 if st.button('인사말 생성'): 
   st.info(a+'님, 안녕하세요 반가워용!!><')
-st.warning(b+'를 꼴에z좋아하시는구먼?')
+st.warning(b+'를 좋아하시는구먼?')
 st.error('반갑웡')
 st.balloons()
